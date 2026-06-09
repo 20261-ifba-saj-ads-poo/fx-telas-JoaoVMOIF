03fec6fe17ca6ea556765f45b19a3b9953b4dde3
JoaoVMOIF <20251tadssaj0007@ifba.edu.br>
2026-06-09 20:00:16 -0300

    Update IndexController.java

--- src/main/java/br/edu/ifba/saj/ads/poo/IndexController.java
+++ src/main/java/br/edu/ifba/saj/ads/poo/IndexController.java
@@ -3,37 +3,30 @@ package br.edu.ifba.saj.ads.poo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class IndexController {
 
-    
-    @FXML
-    private BorderPane pane;
+    @FXML private BorderPane pane;
 
     @FXML
-    public void abrirCadastarFilme(ActionEvent event) {
+    public void abrirCadastrarMedico(ActionEvent event) {
         try {
-            pane.setCenter(FXMLLoader.load(getClass().getResource("Filme.fxml")));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            pane.setCenter(FXMLLoader.load(getClass().getResource("Medico.fxml")));
+        } catch (Exception e) { e.printStackTrace(); }
     }
+
     @FXML
-    public void abrirCadastarSessao(ActionEvent event) {
+    public void abrirAgendarConsulta(ActionEvent event) {
         try {
-            pane.setCenter(FXMLLoader.load(getClass().getResource("Sessao.fxml")));            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            pane.setCenter(FXMLLoader.load(getClass().getResource("Consulta.fxml")));
+        } catch (Exception e) { e.printStackTrace(); }
     }
+
     @FXML
-    public void abrirVenderIngresso(ActionEvent event) {
+    public void abrirVisualizarConsultas(ActionEvent event) {
         try {
-            pane.setCenter(FXMLLoader.load(getClass().getResource("Ingresso.fxml")));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            pane.setCenter(FXMLLoader.load(getClass().getResource("Visualizar.fxml")));
+        } catch (Exception e) { e.printStackTrace(); }
     }
 }
