c5326441708e175944ee89a86c2c486a239be910
JoaoVMOIF <20251tadssaj0007@ifba.edu.br>
2026-06-09 20:02:05 -0300

    Update and rename Cinema.java to ClinicaData.java

---
+++ src/main/java/br/edu/ifba/saj/ads/poo/data/ClinicaData.java
@@ -0,0 +1,9 @@
+package br.edu.ifba.saj.ads.poo.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import br.edu.ifba.saj.ads.poo.model.Medico;
+
+public class ClinicaData {
+    public static List<Medico> medicos = new ArrayList<>();
+}
